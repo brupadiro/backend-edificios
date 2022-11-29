6eb3a716f48f8ea2a5b0e5a806a4ff1d8bd50496
brupadiro <brunodiharce@gmail.com>
2022-11-29 10:45:29 -0300

    ading host and port to envs

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
     client: 'postgres',
     connection: {
       host: env('POSTGRES_HOST'),
-      port: 5432,
+      port: env('POSTGRES_PORT'),
       database: env('POSTGRES_DB'),
       user: env('POSTGRES_USER'),
       password: env('POSTGRES_PASSWORD'),
